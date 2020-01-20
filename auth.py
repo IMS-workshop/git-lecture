23471fddb6d1693ea7a7b7bd9518d3bcef41b8b8
Tiziano Zito <opossumnano@gmail.com>
2020-01-20 13:58:59 +0100

    enabled adding users to the pw db

--- auth.py
+++ auth.py
@@ -14,18 +14,23 @@ def authenticate(username, password, pwdb):
     return False
 
 def read_pwdb(pwdb_file):
+    pwdb_file.seek(0)
     pwdb = pickle.load(pwdb_file)
     return pwdb
 
 def write_pwdb(pwdb, pwdb_file):
+    pwdb_file.seek(0)
     pickle.dump(pwdb, pwdb_file)
 
+def add_user(username, password, pwdb):
+    pwdb[username] = password
+    return pwdb
 
 if __name__ == '__main__':
     DEFAULT_PWDB = 'pwdb.pkl'
 
     try:
-        pwdb_file = open(DEFAULT_PWDB, 'rb')
+        pwdb_file = open(DEFAULT_PWDB, 'rb+')
     except FileNotFoundError:
         pwdb_file = open(DEFAULT_PWDB, 'wb')
         pickle.dump({}, pwdb_file)
@@ -39,5 +44,10 @@ if __name__ == '__main__':
     if authenticate(username, password, pwdb):
         print('Successfull authentication', username, password)
     else:
-        print('User not known or password is wrong!')
+        ans = input('User not known or password is wrong. Do you want to add the '
+                    'user to the password database? [y/n]')
+
+        if ans == 'y':
+            add_user(username, password, pwdb)
+            write_pwdb(pwdb, pwdb_file)
 
