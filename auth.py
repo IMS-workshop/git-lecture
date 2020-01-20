e334c2d0221683d4bea1f4685adf36416cc27a54
Tiziano Zito <opossumnano@gmail.com>
2020-01-20 10:42:29 +0100

    added reading/writing to pw database

--- auth.py
+++ auth.py
@@ -1,9 +1,17 @@
 import getpass
+import pickle
 
 def get_credentials():
     username = input('Enter your username: ')
     password = getpass.getpass('Enter your password: ')
     return username, password
+
+def read_pwdb(pwdb_file):
+    pwdb = pickle.load(pwdb_file)
+    return pwdb
+
+def write_pwdb(pwdb, pwdb_file):
+    pickle.dump(pwdb, pwdb_file)
 
 if __name__ == '__main__':
     username, password = get_credentials()
