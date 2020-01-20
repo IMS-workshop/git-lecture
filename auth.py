22c70438516badcc904585942f6596ec0698d2c5
Tiziano Zito <opossumnano@gmail.com>
2020-01-20 10:43:38 +0100

    create empty db and read it

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 import getpass
 import pickle
+import sys
 
 def get_credentials():
     username = input('Enter your username: ')
@@ -14,5 +15,18 @@ def write_pwdb(pwdb, pwdb_file):
     pickle.dump(pwdb, pwdb_file)
 
 if __name__ == '__main__':
+    DEFAULT_PWDB = 'pwdb.pkl'
+
+    try:
+        pwdb_file = open(DEFAULT_PWDB, 'rb')
+    except FileNotFoundError:
+        pwdb_file = open(DEFAULT_PWDB, 'wb')
+        pickle.dump({}, pwdb_file)
+        pwdb_file.close()
+        print('Created empty pw database!')
+        sys.exit(0)
+
     username, password = get_credentials()
+    pwdb = read_pwdb(pwdb_file)
+
     print(username, password)
